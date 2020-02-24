4461a8ca7db65054ece480b627517110f44a291f
Valeriy Trusevich <truetnoth@gmail.com>
2020-02-24 23:46:18 +0300

    initial

---
+++ script.js
@@ -0,0 +1,19 @@
+let button = document.querySelector(".take_result");
+
+button.addEventListener("click", function() {
+  alert("Код таблицы скопирован в буфер обмена");
+});
+
+function showInstruction() {
+  let container = document.querySelector(".instruction_container");
+  let arrow = document.querySelector(".arrow");
+  if (container.style.display === "block") {
+    container.style.display = "none";
+    arrow.style.backgroundImage = "url(arrow-down.svg)";
+    
+  } else {
+    container.style.display = "block";
+    arrow.style.backgroundImage = "url(arrow-up.svg)";
+  }
+}
+
